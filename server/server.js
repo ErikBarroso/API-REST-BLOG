b6a79218434ea4406dbd0c4440a1793b12aaafe8
Erik Barroso <coderik1996@gmail.com>
2024-01-02 11:58:19 -0300

    application main

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
+require("dotenv").config();
 const app = express();
-
 // middleware definido para quando acessar a rota "/", vão ser chamado os métodos do arquivopostRoute
 app.use("/", require("./route/postsRoute"));
 
