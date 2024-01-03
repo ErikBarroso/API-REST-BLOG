8cc94b055b8aad5e685b56607c060fb98506316a
Erik Barroso <coderik1996@gmail.com>
2024-01-03 06:43:25 -0300

    creating posts, testing and deleting posts so as not to spoil the bank

--- server/teste/posts.test.js
+++ server/teste/posts.test.js
@@ -1,10 +1,35 @@
 const axios = require("axios");
+const crypto = require("crypto");
+const postsService = require("../service/postsService");
+
+const generateText = function () {
+  return crypto.randomBytes(20).toString("hex");
+};
 
 test("Should get posts", async function () {
+  // given - dado que
+  const post1 = await postsService.savePost({
+    title: generateText(),
+    content: generateText(),
+  });
+  const post2 = await postsService.savePost({
+    title: generateText(),
+    content: generateText(),
+  });
+  const post3 = await postsService.savePost({
+    title: generateText(),
+    content: generateText(),
+  });
+  // when - quando acontecer
+
   const response = await axios({
     url: "http://localhost:3000/posts",
     method: "get",
   });
   const posts = response.data;
-  expect(posts).toHaveLength(3)
+  // then - então
+  expect(posts).toHaveLength(3);
+  await postsService.deletePost(post1.id);
+  await postsService.deletePost(post2.id);
+  await postsService.deletePost(post3.id);
 });
