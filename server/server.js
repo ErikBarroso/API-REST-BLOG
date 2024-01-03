7a3eb654579e029ccfd0315aaf7e73dfc6de2a57
Erik Barroso <coderik1996@gmail.com>
2024-01-03 18:47:55 -0300

    added express.json to see request data

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 require("dotenv").config();
 const app = express();
 // middleware definido para quando acessar a rota "/", vão ser chamado os métodos do arquivopostRoute
+app.use(express.json()) // mostrar os dados da request em formato json, 
 app.use("/", require("./route/postsRoute"));
 
 app.listen(3000);
