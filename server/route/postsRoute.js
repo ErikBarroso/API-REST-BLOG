f072272b5bda0b8870a86228038bb2a3bc5b994b
Erik Barroso <coderik1996@gmail.com>
2024-01-03 18:46:52 -0300

    add método the delete, update, getpost and getposts method was added

--- server/route/postsRoute.js
+++ server/route/postsRoute.js
@@ -6,8 +6,19 @@ router.get("/posts", async function (req, res) {
   const posts = await postsService.getPosts();
   res.json(posts);
 });
-router.get("/posts/:id", async function (req, res) {});
-router.post("/posts", async function (req, res) {});
-router.put("posts/:id", async function (req, res) {});
-router.delete("posts/:id", async function (req, res) {});
+router.post("/posts", async function (req, res) {
+  const post = req.body;
+  const newPost = await postsService.savePost(post);
+  res.json(newPost);
+});
+
+router.put("/posts/:id", async function (req, res) {
+  const post = req.body;
+  await postsService.updatePost(req.params.id, post);
+  res.end();
+});
+router.delete("/posts/:id", async function (req, res) {
+  await postsService.deletePost(req.params.id);
+  res.end();
+});
 module.exports = router;
