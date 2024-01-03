8415979195f8d3f20e9eb9f01b12a4ee707b4315
Erik Barroso <coderik1996@gmail.com>
2024-01-03 18:46:17 -0300

    add método the delete, update, getpost and getposts method was added

--- server/data/postsData.js
+++ server/data/postsData.js
@@ -6,6 +6,10 @@ exports.getPosts = function () {
   return database.query("select * from blog.post");
 };
 
+exports.getPost = function (id) {
+  return database.oneOrNone("select * from blog.post where id = $1", [id]);
+};
+
 exports.savePost = function (post) {
   return database.one(
     "insert into blog.post (title, content) values ($1, $2) returning * ",
@@ -15,4 +19,11 @@ exports.savePost = function (post) {
 
 exports.deletePost = function (id) {
   return database.none("delete from blog.post where id = $1", [id]);
+};
+
+exports.updatePost = function (id, post) {
+  return database.none(
+    "update blog.post set title = $1, content = $2 where id = $3",
+    [post.title, post.content, id]
+  );
 };
