9b5635c2d54b53f1362671b7f5c00c68e5735b10
Erik Barroso <coderik1996@gmail.com>
2024-01-03 07:14:24 -0300

    refactoring test

--- server/teste/posts.test.js
+++ server/teste/posts.test.js
@@ -1,31 +1,35 @@
 const axios = require("axios");
 const crypto = require("crypto");
 const postsService = require("../service/postsService");
+const { func } = require("../infra/database");
 
 const generateText = function () {
   return crypto.randomBytes(20).toString("hex");
 };
 
-test("Should get posts", async function () {
+const request = function (url, method, data) {
+return axios({
+    url, method, data})
+}
+
+test.only("Should get posts", async function () {
   // given - dado que
-  const post1 = await postsService.savePost({
+  const post1 = await postsService.savePost( {
     title: generateText(),
-    content: generateText(),
+    content: generateText()
   });
-  const post2 = await postsService.savePost({
+    const post2 = await postsService.savePost( {
     title: generateText(),
-    content: generateText(),
+    content: generateText()
   });
-  const post3 = await postsService.savePost({
+    const post3 = await postsService.savePost( {
     title: generateText(),
-    content: generateText(),
+    content: generateText()
   });
+
   // when - quando acontecer
 
-  const response = await axios({
-    url: "http://localhost:3000/posts",
-    method: "get",
-  });
+  const response = await  request('http://localhost:3000/posts',  'get')
   const posts = response.data;
   // then - então
   expect(posts).toHaveLength(3);
@@ -33,3 +37,4 @@ test("Should get posts", async function () {
   await postsService.deletePost(post2.id);
   await postsService.deletePost(post3.id);
 });
+
