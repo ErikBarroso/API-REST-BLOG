fc81c67480e3188cec03753d802e12f5d462b789
Erik Barroso <coderik1996@gmail.com>
2024-01-03 18:48:05 -0300

    add método the delete, update, getpost and getposts method was added

--- server/service/postsService.js
+++ server/service/postsService.js
@@ -5,10 +5,18 @@ exports.getPosts = function () {
   return postsData.getPosts();
 };
 
+exports.getPost = function (id) {
+  return postsData.getPost(id);
+};
+
 exports.savePost = function (post) {
   return postsData.savePost(post);
 };
 
 exports.deletePost = function (id) {
   return postsData.deletePost(id);
+};
+
+exports.updatePost = function (id, post) {
+  return postsData.updatePost(id, post);
 };
