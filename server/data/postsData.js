5f51f92b2df4957b8687b52f26f92deccd5bec0c
Erik Barroso <coderik1996@gmail.com>
2024-01-03 06:42:32 -0300

    creation of the savepost and deletepost method

--- server/data/postsData.js
+++ server/data/postsData.js
@@ -5,3 +5,14 @@ const database = require("../infra/database");
 exports.getPosts = function () {
   return database.query("select * from blog.post");
 };
+
+exports.savePost = function (post) {
+  return database.one(
+    "insert into blog.post (title, content) values ($1, $2) returning * ",
+    [post.title, post.content]
+  );
+};
+
+exports.deletePost = function (id) {
+  return database.none("delete from blog.post where id = $1", [id]);
+};
