84858b3f1d3bf6d192afa9cd095e4b108b910e6e
Erik Barroso <coderik1996@gmail.com>
2024-01-03 06:42:48 -0300

    creation of the savepost and deletepost method

--- server/service/postsService.js
+++ server/service/postsService.js
@@ -4,3 +4,11 @@ const postsData = require("../data/postsData");
 exports.getPosts = function () {
   return postsData.getPosts();
 };
+
+exports.savePost = function (post) {
+  return postsData.savePost(post);
+};
+
+exports.deletePost = function (id) {
+  return postsData.deletePost(id);
+};
